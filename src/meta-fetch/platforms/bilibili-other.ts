cf182fdd577988e09e31018c28f1e9c7e4141d46
僠儖僲 <108845763+bolanxian@users.noreply.github.com>
2025-11-16 15:00:00 +0800

    已修改        src/meta-fetch/platforms/bilibili-other.ts

--- src/meta-fetch/platforms/bilibili-other.ts
+++ src/meta-fetch/platforms/bilibili-other.ts
@@ -1,32 +1,33 @@
 
 import * as cheerio from 'cheerio'
-import { test, match } from 'bind:utils'
+import { test } from 'bind:utils'
+import { find } from 'bind:Array'
+import { toHttps } from '@/bind'
 import { defineDiscover } from '../discover'
-import { definePlugin } from '../plugin'
-import { $fetch, htmlInit, jsonInit } from '../fetch'
+import { definePlugin, redirectPlugin } from '../plugin'
+import { $fetch, htmlInit } from '../fetch'
 import { cache, json } from '../cache'
 import { REG_INIT } from './bilibili-video'
-import { instantToString } from '@/utils/temporal'
 import { fromHTML } from '@/utils/find-json-object'
 export const REG_CV = /^cv((?!0\d)\d+)$/
-export const REG_DYN = /^bili!dyn!(\d{18,})$/
+export const REG_OPUS = /^bili!opus!(\d{18,})$/
 
 defineDiscover({
   name: 'Bilibili article',
-  discover: [REG_CV],
+  discover: [REG_CV, /^@cv(\d+)$/],
   discoverHttp: [
     /^www\.bilibili\.com\/(?:read\/cv|mobile\?id=)(\d+)/
   ],
   handle: m => `bilibili/article/cv${m[1]}`
 })
 defineDiscover({
-  name: 'Bilibili dynamic',
-  discover: [REG_DYN],
+  name: 'Bilibili opus',
+  discover: [REG_OPUS],
   discoverHttp: [
-    /^t\.bilibili\.com\/(\d{18,})/,
-    /^(?:m|www)\.bilibili\.com\/opus\/(\d{18,})/
+    /^(?:m\.|www\.)?bilibili\.com\/opus\/(\d{18,})/,
+    /^t\.bilibili\.com\/(\d{18,})/
   ],
-  handle: m => `bilibili/dynamic/${m[1]}`
+  handle: m => `bilibili/opus/${m[1]}`
 })
 
 definePlugin({
@@ -37,73 +38,72 @@ definePlugin({
     const id = path[0]
     if (!test(REG_CV, id)) { return }
     return {
-      id, displayId: id, cacheId: id,
+      id: `@${id}`, displayId: id, cacheId: id,
       shortUrl: '', url: `https://www.bilibili.com/read/${id}/`
     }
   },
-  async fetch(info) {
-    const { url, cacheId } = info
-    const text = await cache.tryGet(`${cacheId}.html`, async () => {
-      const resp = await $fetch(url, htmlInit)
-      const { status } = resp
-      if (status !== 200) {
-        throw new TypeError(`Request failed with status code ${status}`)
-      }
-      return await resp.text()
-    })
-    if (text == null) { return }
-    const $ = cheerio.load(text, { baseURI: url })
-    return fromHTML($, REG_INIT)
-  },
-  parse(data, info) {
-    let { shortUrl, url } = info
-    const { readInfo } = data
-    return {
-      title: readInfo.title,
-      ownerName: readInfo.author.name,
-      publishDate: instantToString(readInfo.publish_time * 1000),
-      shortUrl, url,
-      thumbnailUrl: readInfo.banner_url,
-      description: readInfo.summary
-    }
-  }
+  ...redirectPlugin
 })
 definePlugin({
-  name: 'Bilibili dynamic',
-  path: 'bilibili/dynamic',
+  name: 'Bilibili opus',
+  path: 'bilibili/opus',
   resolve(path) {
     if (path.length !== 1) { return }
     const oid = path[0]
-    const id = `bili!dyn!${oid}`
-    if (!test(REG_DYN, id)) { return }
+    const id = `bili!opus!${oid}`
+    if (!test(REG_OPUS, id)) { return }
     return {
-      id, displayId: id, cacheId: id,
-      shortUrl: `https://t.bilibili.com/${oid}`,
+      id, displayId: id, cacheId: id, shortUrl: '',
       url: `https://www.bilibili.com/opus/${oid}`
     }
   },
-  async fetch({ id }) {
-    const oid = match(REG_DYN, id)![1]
+  async fetch({ id, url }) {
     return await json(cache, id, async () => {
-      const data = await (await $fetch(`https://api.bilibili.com/x/polymer/web-dynamic/v1/detail?id=${oid}`, jsonInit)).json()
-      let msg
-      switch (data.code) {
-        case 0: return data.data.item
-        default: msg = `Unknown Error (${data.code}): ${data.message}`; break
+      let text = await cache.get(`${id}.html`)
+      if (text == null) {
+        const resp = await $fetch(url, htmlInit)
+        const { status } = resp
+        if (status !== 200) {
+          throw new TypeError(`Request failed with status code ${status}`)
+        }
+        text = await resp.text()
       }
-      throw new TypeError(msg, { cause: data })
+      const $ = cheerio.load(text, { baseURI: url })
+      const data = fromHTML($, REG_INIT)
+      return data.detail
     })
   },
-  parse(data, info) {
+  parse(detail, info) {
+    let relatedUrl = detail.type === 1
+      ? `https://www.bilibili.com/read/cv${detail.basic.rid_str}`
+      : `https://t.bilibili.com/${detail.id_str}`
     let { shortUrl, url } = info
-    const { module_author: author, module_dynamic: dynamic } = data.modules
+    let content = '', thumb: string | undefined
+    const title = find(detail.modules, m => m.module_type === 'MODULE_TYPE_TITLE')?.module_title.text ?? ''
+    const ownerName = find(detail.modules, m => m.module_type === 'MODULE_TYPE_AUTHOR').module_author.name
+    const { paragraphs } = find(detail.modules, m => m.module_type === 'MODULE_TYPE_CONTENT').module_content
+    for (const p of paragraphs) {
+      switch (p.para_type) {
+        case 1: for (const node of p.text.nodes) {
+          switch (node.type) {
+            case 'TEXT_NODE_TYPE_WORD':
+              content += `${node.word.words}`
+              break
+          }
+        } break
+        case 2: for (const pic of p.pic.pics) {
+          const url = toHttps(pic.url)
+          content += `${url}\n`
+          thumb ??= url
+        } break
+      }
+      content += '\n'
+    }
     return {
-      title: '',
-      ownerName: author.name,
-      publishDate: '',
-      shortUrl, url,
-      thumbnailUrl: dynamic.major?.draw?.items[0].src,
-      description: dynamic.desc.text
+      title, ownerName,
+      shortUrl, url, relatedUrl,
+      thumbnailUrl: thumb,
+      description: content
     }
   }
 })
